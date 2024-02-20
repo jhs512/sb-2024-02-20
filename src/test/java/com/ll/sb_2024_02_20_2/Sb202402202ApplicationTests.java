a535d36e6c1a114d0ffba6296996547c8b9645f6
jhs512 <jangka512@gmail.com>
2024-02-20 15:44:51 +0900

    work

--- src/test/java/com/ll/sb_2024_02_20_2/Sb202402202ApplicationTests.java
+++ src/test/java/com/ll/sb_2024_02_20_2/Sb202402202ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.sb_2024_02_20_2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Sb202402202ApplicationTests {
 
     @Test
