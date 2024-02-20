e4ce1373f545bf32649441b97560b831d6bcbcc3
jhs512 <jangka512@gmail.com>
2024-02-20 10:22:45 +0900

    work

--- src/main/java/com/ll/sb_2024_02_20_2/domain/home/home/controller/HomeController.java
+++ src/main/java/com/ll/sb_2024_02_20_2/domain/home/home/controller/HomeController.java
@@ -1,14 +1,24 @@
 package com.ll.sb_2024_02_20_2.domain.home.home.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    @Value("${custom.jwt.secretKey}")
+    private String jwtSecretKey;
+
     @GetMapping("/")
     @ResponseBody
     public String showMain() {
-        return "홈2";
+        return "홈3";
+    }
+
+    @GetMapping("/jwtSecretKey")
+    @ResponseBody
+    public String showJwtSecretKey() {
+        return jwtSecretKey;
     }
 }
